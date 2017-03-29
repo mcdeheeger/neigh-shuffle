3e20b9b7d62aee44a32599a0b606026929c9f896
Marc Deheeger <mcdeheeger@Marcs-MacBook-Pro.local>
2017-03-28 23:32:30 -0500

    update

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -35,11 +35,11 @@ var latlng = {};
               
               // Setting the coordinates for the neighborhood boundary
               var neighBound = [
-                {lat: results[3].geometry.bounds.f.b, lng: results[3].geometry.bounds.b.b},
-                {lat: results[3].geometry.bounds.f.b, lng: results[3].geometry.bounds.b.f},
-                {lat: results[3].geometry.bounds.f.f, lng: results[3].geometry.bounds.b.f},
-                {lat: results[3].geometry.bounds.f.f, lng: results[3].geometry.bounds.b.b},
-                {lat: results[3].geometry.bounds.f.b, lng: results[3].geometry.bounds.b.b}
+                {lat: results[3].geometry.viewport.f.b, lng: results[3].geometry.viewport.b.b},
+                {lat: results[3].geometry.viewport.f.b, lng: results[3].geometry.viewport.b.f},
+                {lat: results[3].geometry.viewport.f.f, lng: results[3].geometry.viewport.b.f},
+                {lat: results[3].geometry.viewport.f.f, lng: results[3].geometry.viewport.b.b},
+                {lat: results[3].geometry.viewport.f.b, lng: results[3].geometry.viewport.b.b}
               ];
               
               // Drawing the coordinates of the neighborhood boundary
@@ -84,7 +84,7 @@ var latlng = {};
         });
       }
 
-      $("button").on("click", function(event) {
+      $(".neighborhood").on("click", function(event) {
 
       // event.preventDefault();
       console.log("clicked")
@@ -95,12 +95,14 @@ var latlng = {};
         });
         var geocoder = new google.maps.Geocoder();
         // console.log(geocoder)
-        var address = ("chicago" + $(this).attr("value"));
+        var address = ($(this).attr("value") + " chicago");
         // document.getElementById('chicagowickerpark').addEventListener('click', function() {
           geocodeAddress(geocoder, map, address);
         // });
       
         function geocodeAddress(geocoder, resultsMap, address) {
+
+
         
         console.log(address, "this")
         geocoder.geocode({'address': address}, function(results, status) {
@@ -109,11 +111,11 @@ var latlng = {};
             resultsMap.setCenter({lat: 41.8781, lng: -87.669});
             
             var neighBound = [
-                {lat: results[0].geometry.bounds.f.b, lng: results[0].geometry.bounds.b.b},
-                {lat: results[0].geometry.bounds.f.b, lng: results[0].geometry.bounds.b.f},
-                {lat: results[0].geometry.bounds.f.f, lng: results[0].geometry.bounds.b.f},
-                {lat: results[0].geometry.bounds.f.f, lng: results[0].geometry.bounds.b.b},
-                {lat: results[0].geometry.bounds.f.b, lng: results[0].geometry.bounds.b.b}
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.b},
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.f},
+                {lat: results[0].geometry.viewport.f.f, lng: results[0].geometry.viewport.b.f},
+                {lat: results[0].geometry.viewport.f.f, lng: results[0].geometry.viewport.b.b},
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.b}
               ];
               
               // Drawing the coordinates of the neighborhood boundary
@@ -151,3 +153,81 @@ var latlng = {};
         });
       }
   });
+ 
+ 
+
+ $("#shuffle").on("click", function(event) {
+
+
+
+      event.preventDefault();
+      console.log("clicked")
+        
+        var map = new google.maps.Map(document.getElementById('google-map'), {
+          zoom: 10,
+          center: {lat: 41.8781, lng: -87.669}
+        });
+        var geocoder = new google.maps.Geocoder();
+        // console.log(geocoder)
+        var shuffNeigh =  ("chicago " + neighborhoodList[Math.floor(Math.random() * neighborhoodList.length)]);
+    console.log("shuffled neighborhood", shuffNeigh)
+        
+          geocodeAddress(geocoder, map, shuffNeigh);
+          search("chicago " + shuffNeigh);
+      
+        function geocodeAddress(geocoder, resultsMap, shuffNeigh) {
+
+
+        
+        console.log(shuffNeigh, "this")
+        geocoder.geocode({'address': shuffNeigh}, function(results, status) {
+          console.log("results", results)
+          if (status === 'OK') {
+            resultsMap.setCenter({lat: 41.8781, lng: -87.669});
+            
+           var neighBound = [
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.b},
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.f},
+                {lat: results[0].geometry.viewport.f.f, lng: results[0].geometry.viewport.b.f},
+                {lat: results[0].geometry.viewport.f.f, lng: results[0].geometry.viewport.b.b},
+                {lat: results[0].geometry.viewport.f.b, lng: results[0].geometry.viewport.b.b}
+              ];
+              
+              // Drawing the coordinates of the neighborhood boundary
+              var neighBound = new google.maps.Polyline({
+                path: neighBound,
+                geodesic: true,
+                strokeColor: 'blue',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+              });
+
+              // Creating the neighborhood boundary map
+              neighBound.setMap(map);
+
+              var imageBounds = {
+                north: 42.023131,
+                south: 41.6443349,
+                east: -87.523661,
+                west: -87.94026689999998
+              };
+
+              var overlayOpts = {
+                opacity:.6
+              }
+
+              historicalOverlay = new google.maps.GroundOverlay(
+                  'assets/images/chicago.png',
+                  imageBounds, overlayOpts);
+              
+              historicalOverlay.setMap(map);
+
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
+  });
+ 
+
+    
